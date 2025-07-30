4b7468a5c12a581da7391520dc693a04c2e7c34a
amir ahmadi <162714606+dukeahmadi@users.noreply.github.com>
2025-07-30 16:25:34 +0330

    app/main.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import numpy as np
+import pickle
+
+# مدل رو بارگذاری کن
+with open("model_lgbm_diabetes.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = FastAPI()
+
+# مدل داده ورودی با Pydantic
+class Features(BaseModel):
+    features: list[float]
+
+@app.get("/")
+def root():
+    return {"message": "API is running!"}
+
+@app.post("/predict")
+def predict(data: Features):
+    arr = np.array(data.features).reshape(1, -1)
+    prediction = model.predict(arr)
+    return {"prediction": int(prediction[0])}
